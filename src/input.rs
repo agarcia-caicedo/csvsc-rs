0a3a33b72ee6bdb430536a1cae9fb3eae0956ce8
Abraham Toriz <awonderfulcode@gmail.com>
2019-01-30 10:52:13 -0600

    this level of generic code was not required

--- src/input.rs
+++ src/input.rs
@@ -1,22 +1,24 @@
 use std::iter::FromIterator;
 use csv::ByteRecord;
+use csv::Reader;
 
 pub struct InputStream<T> {
-    array: Vec<T>,
+    array: Vec<Reader<T>>,
+    current: usize,
 }
 
 impl<T> InputStream<T> {
     fn new() -> InputStream<T> {
-        InputStream{ array: Vec::new() }
+        InputStream{ array: Vec::new(), current: 0 }
     }
 
-    fn add(&mut self, item: T) {
+    fn add(&mut self, item: Reader<T>) {
         self.array.push(item);
     }
 }
 
-impl<T> FromIterator<T> for InputStream<T> {
-    fn from_iter<I: IntoIterator<Item=T>>(iter: I) -> Self {
+impl<T> FromIterator<Reader<T>> for InputStream<T> {
+    fn from_iter<I: IntoIterator<Item=Reader<T>>>(iter: I) -> Self {
         let mut ra:InputStream<T> = InputStream::new();
 
         for item in iter {
