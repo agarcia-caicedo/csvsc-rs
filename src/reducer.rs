c213e736462db9a76370f1440f3a06b4fd3c1d60
Abraham Toriz <awonderfulcode@gmail.com>
2019-02-16 13:41:08 -0600

    redesign how the reducer is going to be consumed

--- src/reducer.rs
+++ src/reducer.rs
@@ -1,5 +1,5 @@
 use std::str::FromStr;
-use std::collections::VecDeque;
+use std::collections::HashMap;
 use super::{Headers, RowStream, RowResult};
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
@@ -8,14 +8,17 @@ use std::hash::{Hash, Hasher};
 pub enum AggregateParseError {
 }
 
-pub struct Aggregate {
+pub enum Aggregate {
+    Sum{
+        colname: String,
+    },
 }
 
 impl FromStr for Aggregate {
     type Err = AggregateParseError;
 
     fn from_str(spec: &str) -> Result<Aggregate, Self::Err> {
-        Ok(Aggregate{})
+        unimplemented!()
     }
 }
 
@@ -24,12 +27,16 @@ pub enum ReducerBuildError {
     KeyError(String),
 }
 
+enum ReducerState {
+    Processed,
+    Unprocessed,
+}
+
 pub struct Reducer<T> {
     iter: T,
     group_by: Vec<String>,
     columns: Vec<Aggregate>,
     headers: Headers,
-    contents: VecDeque<RowResult>,
 }
 
 impl<T> Reducer<T>
@@ -48,7 +55,9 @@ impl<T> Reducer<T>
         }
 
         for col in columns.iter() {
-            unimplemented!();
+            headers.add(match col {
+                Aggregate::Sum{colname, ..} => colname,
+            });
         }
 
         Ok(Reducer {
@@ -56,7 +65,6 @@ impl<T> Reducer<T>
             group_by,
             columns,
             headers,
-            contents: VecDeque::new(),
         })
     }
 }
@@ -67,7 +75,21 @@ impl<I> RowStream for Reducer<I> {
     }
 }
 
-impl<T: Iterator<Item = RowResult>> Iterator for Reducer<T> {
+impl<I> IntoIterator for Reducer<I> {
+    type Item = RowResult;
+    type IntoIter = IntoIter;
+
+    /// Calling this triggers consumption of the source iterator and consumes
+    /// the reducer itself, yielding an interator over the aggregated results.
+    fn into_iter(self) -> Self::IntoIter {
+        unimplemented!()
+    }
+}
+
+pub struct IntoIter {
+}
+
+impl Iterator for IntoIter {
     type Item = RowResult;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -90,7 +112,7 @@ mod tests {
             Ok(Row::from(vec!["c", "a"])),
         ].into_iter()).unwrap();
 
-        let mut r = Reducer::new(iter, Vec::new(), Vec::new()).unwrap();
+        let mut r = Reducer::new(iter, Vec::new(), Vec::new()).unwrap().into_iter().into_iter();
 
         assert_eq!(r.next().unwrap().unwrap(), Row::from(vec!["a"]));
         assert_eq!(r.next().unwrap().unwrap(), Row::from(vec!["b"]));
@@ -107,7 +129,7 @@ mod tests {
             Ok(Row::from(vec!["2", "9", "a"])),
         ].into_iter()).unwrap();
 
-        let mut r = Reducer::new(iter, vec!["0"], vec!["avg:1".parse().unwrap()]).unwrap();
+        let mut r = Reducer::new(iter, vec!["0"], vec!["avg:1".parse().unwrap()]).unwrap().into_iter();
 
         assert_eq!(r.next().unwrap().unwrap(), Row::from(vec!["1", "2", "3.0"]));
         assert_eq!(r.next().unwrap().unwrap(), Row::from(vec!["2", "7", "8.0"]));
@@ -123,7 +145,7 @@ mod tests {
             Ok(Row::from(vec!["2", "9", "a"])),
         ].into_iter()).unwrap();
 
-        let mut r = Reducer::new(iter, vec!["0"], vec!["min:1".parse().unwrap()]).unwrap();
+        let mut r = Reducer::new(iter, vec!["0"], vec!["min:1".parse().unwrap()]).unwrap().into_iter();
 
         assert_eq!(r.next().unwrap().unwrap(), Row::from(vec!["1", "2", "2.0"]));
         assert_eq!(r.next().unwrap().unwrap(), Row::from(vec!["2", "7", "7.0"]));
@@ -139,7 +161,7 @@ mod tests {
             Ok(Row::from(vec!["2", "9", "a"])),
         ].into_iter()).unwrap();
 
-        let mut r = Reducer::new(iter, vec!["0"], vec!["max:1".parse().unwrap()]).unwrap();
+        let mut r = Reducer::new(iter, vec!["0"], vec!["max:1".parse().unwrap()]).unwrap().into_iter();
 
         assert_eq!(r.next().unwrap().unwrap(), Row::from(vec!["1", "2", "4.0"]));
         assert_eq!(r.next().unwrap().unwrap(), Row::from(vec!["2", "7", "9.0"]));
@@ -155,7 +177,7 @@ mod tests {
             Ok(Row::from(vec!["2", "9", "a"])),
         ].into_iter()).unwrap();
 
-        let mut r = Reducer::new(iter, vec!["0"], vec!["sum:1".parse().unwrap()]).unwrap();
+        let mut r = Reducer::new(iter, vec!["0"], vec!["sum:1".parse().unwrap()]).unwrap().into_iter();
 
         assert_eq!(r.next().unwrap().unwrap(), Row::from(vec!["1", "2", "6.0"]));
         assert_eq!(r.next().unwrap().unwrap(), Row::from(vec!["2", "7", "16.0"]));
