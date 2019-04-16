3c8c230827107726552a039de8f3edb3665606e5
Abraham Toriz <awonderfulcode@gmail.com>
2019-04-16 13:04:22 -0400

    adjust accesiblity of modules, this will probably break things

--- src/lib.rs
+++ src/lib.rs
@@ -84,8 +84,7 @@ mod adjacent_reduce;
 mod row_stream;
 mod del;
 mod adjacent_sort;
-
-pub mod mock;
+mod mock;
 
 pub use add::{Add, ColSpec};
 pub use error::{Error, RowResult};
@@ -96,11 +95,12 @@ pub use inspect::Inspect;
 pub use reducer::Reducer;
 pub use row_stream::{get_field, RowStream};
 pub use add_with::AddWith;
-pub use adjacent_reduce::AdjacentReduce;
 pub use del::Del;
 // TODO rethink this whole adjacent thing... it might be a good idea to abstract
-// it into something better
+// it into something better that calls a closure with a vector of adjacent rows
+// for example, then implement stuff on top of that
 pub use adjacent_sort::AdjacentSort;
+pub use adjacent_reduce::AdjacentReduce;
 
 /// Type alias of csv::StringRecord. Represents a row of data.
 pub type Row = csv::StringRecord;
