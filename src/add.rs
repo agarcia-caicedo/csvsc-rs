e688ce3dac179c643a29b9766cb90910c4cc6fb0
Abraham Toriz <awonderfulcode@gmail.com>
2019-04-26 11:48:09 -0400

    extract colspec into its own module

--- src/add.rs
+++ src/add.rs
@@ -1,165 +1,11 @@
-//! Utilities for adding rows
-use regex::{Captures, Regex};
-use std::str::FromStr;
-use strfmt::{strfmt_map, FmtError, Formatter};
-
 use crate::{
-    Headers, Row, RowStream,
+    Headers, RowStream,
     error::{Error, RowResult},
-    col,
 };
 
-/// Clases de errores que pueden pasar al interpretar la especificación de
-/// cómo construir una nueva columna.
-#[derive(Debug)]
-pub enum ColSpecParseError {
-    MissingSource,
-    MissingColname,
-    MissingColDef,
-    MissingRegex,
-    InvalidRegex,
-    InvalidSpec,
-}
+mod colspec;
 
-fn interpolate(template: &str, captures: &Captures) -> String {
-    let mut res = String::new();
-
-    captures.expand(template, &mut res);
-
-    res
-}
-
-/// Tipos de especificaciones disponibles para crear una nueva columna.
-pub enum ColSpec {
-    /// Construye una nueva columna basándose en una columna anterior, usando
-    /// una expresión regular para extraer información de la misma.
-    Regex {
-        source: String,
-        colname: String,
-        coldef: String,
-        regex: Regex,
-    },
-
-    /// Crea una nueva columna mezclando columnas existentes mediante el uso de
-    /// una plantilla
-    ///
-    /// E.g. suponiendo que existen las columnas `month` y `day`:
-    ///
-    /// ```rust
-    /// use csvsc::ColSpec;
-    ///
-    /// let spec = ColSpec::Mix {
-    ///     colname: "new_col".to_string(),
-    ///     coldef: "{day}/{month}".to_string(),
-    /// };
-    Mix { colname: String, coldef: String },
-}
-
-impl ColSpec {
-    pub fn compute(&self, data: &Row, headers: &Headers) -> Result<String, col::BuildError> {
-        match *self {
-            ColSpec::Mix { ref coldef, .. } => match strfmt_map(&coldef, &|mut fmt: Formatter| {
-                let v = match headers.get_field(data, fmt.key) {
-                    Some(v) => v,
-                    None => {
-                        return Err(FmtError::KeyError(fmt.key.to_string()));
-                    }
-                };
-                fmt.str(v)
-            }) {
-                Ok(s) => Ok(s),
-                Err(FmtError::Invalid(_)) => Err(col::BuildError::InvalidFormat),
-                Err(FmtError::KeyError(s)) => Err(col::BuildError::KeyError(s)),
-                Err(FmtError::TypeError(_)) => Err(col::BuildError::InvalidFormat),
-            },
-            ColSpec::Regex {
-                ref source,
-                ref coldef,
-                ref regex,
-                ..
-            } => match headers.get_field(data, source) {
-                Some(field) => match regex.captures(field) {
-                    Some(captures) => Ok(interpolate(&coldef, &captures)),
-                    None => Err(col::BuildError::ReNoMatch(regex.clone(), field.to_string())),
-                },
-                None => Err(col::BuildError::KeyError(source.to_string())),
-            },
-        }
-    }
-}
-
-impl FromStr for ColSpec {
-    type Err = ColSpecParseError;
-
-    fn from_str(spec: &str) -> Result<ColSpec, Self::Err> {
-        if spec.starts_with("regex:") {
-            let mut pieces = spec.split(':');
-            pieces.next();
-
-            let source;
-            let colname;
-            let coldef;
-            let regex;
-
-            if let Some(s) = pieces.next() {
-                source = s.to_string();
-            } else {
-                return Err(ColSpecParseError::MissingSource);
-            }
-
-            if let Some(s) = pieces.next() {
-                colname = s.to_string();
-            } else {
-                return Err(ColSpecParseError::MissingColname);
-            }
-
-            if let Some(s) = pieces.next() {
-                coldef = s.to_string();
-            } else {
-                return Err(ColSpecParseError::MissingColDef);
-            }
-
-            if let Some(s) = pieces.next() {
-                if let Ok(r) = Regex::new(s) {
-                    regex = r;
-                } else {
-                    return Err(ColSpecParseError::InvalidRegex);
-                }
-            } else {
-                return Err(ColSpecParseError::MissingRegex);
-            }
-
-            Ok(ColSpec::Regex {
-                source,
-                colname,
-                coldef,
-                regex,
-            })
-        } else if spec.starts_with("value:") {
-            let mut pieces = spec.split(':');
-            pieces.next();
-
-            let colname;
-            let coldef;
-
-            if let Some(s) = pieces.next() {
-                colname = s.to_string();
-            } else {
-                return Err(ColSpecParseError::MissingColname);
-            }
-
-            if let Some(s) = pieces.next() {
-                coldef = s.to_string();
-            } else {
-                return Err(ColSpecParseError::MissingColDef);
-            }
-
-            Ok(ColSpec::Mix { colname, coldef })
-        } else {
-            Err(ColSpecParseError::InvalidSpec)
-        }
-    }
-}
+pub use colspec::ColSpec;
 
 #[derive(Debug)]
 pub enum BuildError {
@@ -256,7 +102,7 @@ where
 
 #[cfg(test)]
 mod tests {
-    use super::{interpolate, Add, ColSpec, Headers, Regex, Row, RowStream};
+    use super::{Add, ColSpec, Headers, Regex, Row, RowStream};
     use crate::{
         SOURCE_FIELD,
         mock::MockStream,
@@ -383,19 +229,6 @@ mod tests {
         assert_eq!(
             add.next().unwrap().unwrap(),
             Row::from(vec!["3", "1"])
-        );
-    }
-
-    #[test]
-    fn test_interpolate() {
-        let regex =
-            Regex::new(r"(?P<year>[0-9]{4})-(?P<month>[0-9]{2})-(?P<day>[0-9]{2})").unwrap();
-        let captures = regex.captures("2019-02-09").unwrap();
-        let template = String::from("Día: ${day} mes: ${month} año: ${year}");
-
-        assert_eq!(
-            interpolate(&template, &captures),
-            "Día: 09 mes: 02 año: 2019"
         );
     }
 }
